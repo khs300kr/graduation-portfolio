c74b689bdafcba6bfa72eb9b7ecde25685ac698c
KIM HUISEUNG <khs300kr@naver.com>
2017-05-09 05:46:24 +0900

    애니메이션동기화(테스트)

---
+++ 김희승/애니메이션동기화(테스트)/Lunatic Project/LunaticProject/Scene.h
@@ -0,0 +1,91 @@
+#pragma once
+#include "Camera.h"
+#include "Shader.h"
+#include "SkyBoxShader.h"
+
+#include "SordMan.h"
+
+#define MAX_LIGHTS		4 
+#define POINT_LIGHT		1.0f
+#define SPOT_LIGHT		2.0f
+#define DIRECTIONAL_LIGHT	3.0f
+
+
+// 1개의 조명을 표현하는 구조체이다. 
+struct LIGHT
+{
+	D3DXCOLOR m_d3dxcAmbient;
+	D3DXCOLOR m_d3dxcDiffuse;
+	D3DXCOLOR m_d3dxcSpecular;
+	D3DXVECTOR3 m_d3dxvPosition;
+	float m_fRange;
+	D3DXVECTOR3 m_d3dxvDirection;
+	float m_nType;
+	D3DXVECTOR3 m_d3dxvAttenuation;
+	float m_fFalloff;
+	float m_fTheta; //cos(m_fTheta)
+	float m_fPhi; //cos(m_fPhi)
+	float m_bEnable;
+	float padding;
+};
+
+// 상수 버퍼는 크기가 반드시 16 바이트의 배수가 되어야 한다. 
+struct LIGHTS
+{
+	LIGHT m_pLights[MAX_LIGHTS];
+	D3DXCOLOR m_d3dxcGlobalAmbient;
+	D3DXVECTOR4 m_d3dxvCameraPosition;
+};
+
+class CScene
+{
+private:
+	int m_nShaders;
+	int m_nInstancingShaders;
+
+	
+	CInstancingShader **m_ppInstancingShaders;
+
+	CCamera *m_pCamera;
+
+	LIGHTS *m_pLights;
+	ID3D11Buffer *m_pd3dcbLights;
+
+	bool LeftKeyDown;
+	bool RightKeyDown;
+	bool UpKeyDown;
+	bool DownKeyDown;
+
+	int KeyDownForServer;
+	DWORD dwDirection;
+
+
+public:
+	CScene();
+	~CScene();
+	CSordMan* pSordmanObject;
+	CSordMan* pOtherObject[MAX_USER];
+	CShader **m_ppShaders;
+	
+	bool OnProcessingMouseMessage(HWND hWnd, UINT nMessageID, WPARAM wParam, LPARAM lParam);
+	bool OnProcessingKeyboardMessage(HWND hWnd, UINT nMessageID, WPARAM wParam, LPARAM lParam);
+
+	void BuildObjects(ID3D11Device *pd3dDevice);
+	void ReleaseObjects();
+
+	void ProcessInput();
+	void AnimateObjects(float fTimeElapsed);
+	void Render(ID3D11DeviceContext*pd3dDeviceContext, CCamera *pCamera);
+
+	void SetCamera(CCamera *pCamera) { m_pCamera = pCamera; }
+
+	void CreateShaderVariables(ID3D11Device *pd3dDevice);
+	void UpdateShaderVariable(ID3D11DeviceContext *pd3dDeviceContext, LIGHTS *pLights);
+	void ReleaseShaderVariables();
+
+public:
+	// server
+	DWORD Animation_number[MAX_USER] = {};
+	void SendMovePacket(BYTE type);
+
+};
---
+++ 김희승/애니메이션동기화(테스트)/Lunatic Project/LunaticProject/stdafx.h
@@ -0,0 +1,82 @@
+// stdafx.h : 자주 사용하지만 자주 변경되지는 않는
+// 표준 시스템 포함 파일 및 프로젝트 관련 포함 파일이
+// 들어 있는 포함 파일입니다.
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN             // 거의 사용되지 않는 내용은 Windows 헤더에서 제외합니다.
+// server
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#define INITGUID
+#include <WinSock2.h>
+#include "../../Lunatic_Server/Lunatic_Server/protocol.h"
+#pragma comment (lib, "ws2_32.lib")
+
+#define	BUF_SIZE				1024
+#define	WM_SOCKET				WM_USER + 1
+
+#include "ServerGlobal.h"
+
+// Windows 헤더 파일:
+#include <windows.h>
+
+// C 런타임 헤더 파일입니다.
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+
+
+// TODO: 프로그램에 필요한 추가 헤더는 여기에서 참조합니다.
+// DX11 전용 헤더
+#include <D3D11.h>
+#include <D3DX11.h>
+#include <mmsystem.h>
+#include <xnamath.h>
+#include <math.h>
+#include <d3dcompiler.h>
+#include <D3DX10math.h>
+#include <stdio.h>
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+#define FRAME_BUFFER_WIDTH	1024
+#define FRAME_BUFFER_HEIGHT	768
+
+#define RANDOM_COLOR	D3DXCOLOR((rand() * 0xFFFFFF / RAND_MAX))
+#define ASPECT_RATIO	(float(FRAME_BUFFER_WIDTH) / float(FRAME_BUFFER_HEIGHT))
+
+// 테스트를 위한 콘솔 띄우기
+#pragma comment(linker, "/entry:wWinMainCRTStartup /subsystem:console")
+
+#define KEY_DOWN(vk_code) ((GetAsyncKeyState(vk_code) & 0x8000) ? 1 : 0)
+#define KEY_UP(vk_code)   ((GetAsyncKeyState(vk_code) & 0x8000) ? 0 : 1)
+
+//
+// 쉐이더 슬롯
+//////////////////////////////////////////
+#define VS_SLOT_CAMERA			0x00
+#define VS_SLOT_WORLD_MATRIX	0x01
+#define VS_SLOT_SKINNEDBONE		0x02
+
+// 조명과 재질 설정용 상수 버퍼 슬롯
+#define PS_SLOT_LIGHT			0x00
+#define PS_SLOT_MATERIAL		0x01
+
+// Texture & Sampler State 설정을 위한 슬롯
+#define PS_SLOT_TEXTURE			0x00
+#define PS_SLOT_SAMPLER_STATE	0x00
+
+
+#define ANIFRAMETIME			0.0333333f
+#define GetFBXMesh				GetGameObject(0)->GetMesh(0)
+
